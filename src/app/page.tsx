0317ce5d5d05895590a80a7ceda131a626529566
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-03-06 21:41:18 +0300

    Перенесла переиспользуемый код в layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,11 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import { Header } from "../components/Header/Header";
 import { Centerblock } from "../components/Centerblock/Centerblock";
 import { Sidebar } from "../components/Sidebar/Sidebar";
-import { Bar } from "../components/Bar/Bar";
 
 export default function Home() {
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Header />
-          <Centerblock />
-          <Sidebar />
-        </main>
-        <Bar />
-        <footer />
-      </div>
-    </div>
+    <>
+      <Centerblock />
+      <Sidebar />
+    </>
   );
 }
