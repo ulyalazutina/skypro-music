4c04abbee69ec31e580b0b4529911105c24c05e3
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-03-19 22:59:26 +0300

    Заглушка для фильтров

--- src/components/Filter/Filter.tsx
+++ src/components/Filter/Filter.tsx
@@ -2,6 +2,7 @@
 import { FilterItem } from "@components/FilterItem/FilterItem";
 import styles from "./Filter.module.css";
 import { useState } from "react";
+import { authors, genres, years } from "./data";
 
 export function Filter() {
   const [activeFilter, setActiveFilter] = useState<string | null>(null);
@@ -12,13 +13,13 @@ export function Filter() {
   return (
     <div className={styles.centerblockFilter}>
       <div className={styles.filterTitle}>Искать по:</div>
-      <FilterItem onClick={() => handleFilterClick("authors")} isOpened={activeFilter === "authors"}>
+      <FilterItem onClick={() => handleFilterClick("authors")} isOpened={activeFilter === "authors"} list={authors}>
         исполнителю
       </FilterItem>
-      <FilterItem onClick={() => handleFilterClick("years")} isOpened={activeFilter === "years"}>
+      <FilterItem onClick={() => handleFilterClick("years")} isOpened={activeFilter === "years"} list={years}>
         году выпуска
       </FilterItem>
-      <FilterItem onClick={() => handleFilterClick("genres")} isOpened={activeFilter === "genres"}>
+      <FilterItem onClick={() => handleFilterClick("genres")} isOpened={activeFilter === "genres"} list={genres}>
         жанру
       </FilterItem>
     </div>
--- src/components/FilterItem/FilterItem.tsx
+++ src/components/FilterItem/FilterItem.tsx
@@ -2,9 +2,14 @@
 import classNames from "classnames";
 import styles from "./FilterItem.module.css";
 
-type FilterItemProps = { children: string; onClick: () => void; isOpened: boolean };
+type FilterItemProps = {
+  children: string;
+  onClick: () => void;
+  isOpened: boolean;
+  list: Array<{ id: number; name: string }>;
+};
 
-export function FilterItem({ children, onClick, isOpened }: FilterItemProps) {
+export function FilterItem({ children, onClick, isOpened, list }: FilterItemProps) {
   return (
     <div className={styles.container}>
       <div onClick={onClick} className={classNames(styles.filterButton, styles.btnText)}>
@@ -13,12 +18,9 @@ export function FilterItem({ children, onClick, isOpened }: FilterItemProps) {
       {isOpened && (
         <div className={styles.wrapper}>
           <ul className={styles.items}>
-            <li className={styles.item}>Frank Sinatra</li>
-            <li className={styles.item}>Frank Sinatra</li>
-            <li className={styles.item}>Frank Sinatra</li>
-            <li className={styles.item}>Frank Sinatra</li>
-            <li className={styles.item}>Frank Sinatra</li>
-            <li className={styles.item}>Frank Sinatra</li>
+            {list.map((item) => (
+              <li className={styles.item} key={item.id}>{item.name}</li>
+            ))}
           </ul>
         </div>
       )}
