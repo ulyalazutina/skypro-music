d776831cbb2dbcb16c0c802c3bb0727a82a642de
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-03-21 13:25:35 +0300

    воспроизведение/пауза при нажатии на обложку трека

--- src/components/Song/Song.tsx
+++ src/components/Song/Song.tsx
@@ -1,11 +1,28 @@
+"use client";
+import { useRef, useState } from "react";
 import styles from "./Song.module.css";
 
 export default function Song({ item }: { item: trackType }) {
+  const audioRef = useRef<any>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
+
+  const togglePlay = () => {
+    const audio = audioRef.current;
+    console.log(audio);
+    if (isPlaying) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+    setIsPlaying((prev) => !prev);
+  };
+  
   return (
     <div className={styles.playlistItem}>
+      <audio ref={audioRef} src={item.track_file}></audio>
       <div className={styles.playlistTrack}>
         <div className={styles.trackTitle}>
-          <div className={styles.trackTitleImage}>
+          <div className={styles.trackTitleImage} onClick={togglePlay}>
             <svg className={styles.trackTitleSvg}>
               <use xlinkHref="/image/icon/sprite.svg#icon-note" />
             </svg>
