7bc81e08355f6375f580292ba9bba68a25c7ee29
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-03-25 09:02:06 +0300

    Компонент Main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,12 @@
-import { Header } from "@components/Header/Header";
 import styles from "./page.module.css";
-import { Search } from "@components/Search/Search";
-import { Filter } from "@components/Filter/Filter";
-import { Column } from "@components/Column/Column";
-import { Sidebar } from "@components/Sidebar/Sidebar";
 import { Bar } from "@components/Bar/Bar";
-import Playlist from "@components/Playlist/Playlist";
+import Main from "@components/Main/Main";
 
 export default function Home() {
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
-        <main className={styles.main}>
-          <Header />
-          <div className={styles.mainCenterblock}>
-            <Search />
-            <h2 className={styles.centerblockTitle}>Треки</h2>
-            <Filter />
-            <div className={styles.centerblockContent}>
-              <Column />
-              <Playlist />
-            </div>
-          </div>
-          <Sidebar />
-        </main>
+        <Main />
         <Bar />
         <footer />
       </div>
