3c09f1bd3614ac9333e6c4e4631ab86f6e9302ef
Ulyana Lazutina <ulyalazutina@yandex.ru>
2024-04-15 12:16:51 +0300

    Тестирование функции форматирования времени

--- src/app/(pages)/tracks/layout.tsx
+++ src/app/(pages)/tracks/layout.tsx
@@ -2,7 +2,7 @@ import styles from "./page.module.css";
 
 export default function TracksLayout({children}: Readonly<{
     children: React.ReactNode;
-  }>) {
+}>) {
     return (
         <div className={styles.wrapper}>
             <div className={styles.container}>
